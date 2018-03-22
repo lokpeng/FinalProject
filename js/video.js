56e34a2b36f138e81ae023fe0343902f63994183
casslim <lokpeng1998717@gmail.com>
2018-03-22 20:07:53 +0800

    Initial commit

---
+++ js/video.js
@@ -0,0 +1,65 @@
+function show_confirm()
+{
+var r=confirm("你确定要执行此操作吗？");
+if (r==true)
+  {
+  alert("即将跳转到 www.03pj.com");
+  window.location = "http://www.03pj.com/";
+  }
+else
+  {
+  alert("你取消了此项操作");
+  }
+}
+function show_confirm2()
+{
+var r=confirm("你确定要执行此操作吗？");
+if (r==true)
+  {
+  alert("即将跳转到 http://www.03pj.com/?m=vod-play-id-98436-src-1-num-1.html");
+  window.location = "http://www.03pj.com/?m=vod-play-id-98436-src-1-num-1.html";
+  }
+else
+  {
+  alert("你取消了此项操作");
+  }
+}
+function show_confirm3()
+{
+var r=confirm("你确定要执行此操作吗？");
+if (r==true)
+  {
+  alert("即将跳转到 http://www.03pj.com/?m=vod-play-id-98412-src-1-num-1.html");
+  window.location = "http://www.03pj.com/?m=vod-play-id-98412-src-1-num-1.html";
+  }
+else
+  {
+  alert("你取消了此项操作");
+  }
+}
+function show_confirm4()
+{
+var r=confirm("你确定要执行此操作吗？");
+if (r==true)
+  {
+  alert("即将跳转到 http://www.03pj.com/?m=vod-play-id-97464-src-1-num-1.html");
+  window.location = "http://www.03pj.com/?m=vod-play-id-97464-src-1-num-1.html";
+  }
+else
+  {
+  alert("你取消了此项操作");
+  }
+}
+function show_confirm5()
+{
+var r=confirm("你确定要执行此操作吗？");
+if (r==true)
+  {
+  alert("即将跳转到http://www.03pj.com/?m=vod-play-id-97146-src-1-num-1.html");
+  window.location = "http://www.03pj.com/?m=vod-play-id-97146-src-1-num-1.html";
+  }
+else
+  {
+  alert("你取消了此项操作");
+  }
+}
